19bee86ff7dab66eb4b4a361061f2a06250b72d0
Vincent Rousseau <vincent.rousseau.pro@gmail.com>
2025-11-24 13:32:55 +0100

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 // vitest.config.ts
 import { defineConfig, configDefaults } from "vitest/config";
+import { fileURLToPath } from "node:url";
+
+const setupFile = fileURLToPath(new URL("./tests/setup.js", import.meta.url));
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     environmentMatchGlobs: [["tests/unit/locales.snap.spec.*", "node"]],
-    setupFiles: ["tests/setup.js"],
+    setupFiles: [setupFile],
     globals: true,
     exclude: [...configDefaults.exclude, "tests/e2e/**", "tests/perf/**"],
   },
